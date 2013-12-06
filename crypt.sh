be4ff9f1805062aad711c34cc6f2fc2574bd4a60
Ralph Yozzo <fedex1@gmail.com>
2013-12-06 00:18:44 -0500

    Add more test cases

--- crypt.sh
+++ crypt.sh
@@ -1,2 +1,3 @@
- openssl smime -encrypt -binary -aes-256-cbc -in main.py -out main.py.enc -outform DER brooklynmarathon.pem
- openssl smime -decrypt -binary -in main.py.enc -inform DER -out x.py -inkey brooklynmarathon.pem 
+#!/usr/bin/env bash
+openssl smime -encrypt -binary -aes-256-cbc -in main.py -out main.py.enc -outform DER brooklynmarathon.pem
+openssl smime -decrypt -binary -in main.py.enc -inform DER -out x.py -inkey brooklynmarathon.pem 
